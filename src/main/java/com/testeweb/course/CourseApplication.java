4057146e70c43d8e58bffcf012b39e779e637689
Danielscript1 <danielsousagit@gmail.com>
2021-11-10 11:41:22 -0300

    User entity and resource

--- src/main/java/com/testeweb/course/CourseApplication.java
+++ src/main/java/com/testeweb/course/CourseApplication.java
@@ -3,11 +3,15 @@ package com.testeweb.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+import com.testeweb.course.entities.User;
+
 @SpringBootApplication
 public class CourseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
+		
 	}
 
 }
