bd6aa43aec8271ed00d9cc954d3fb670497942b9
Danielscript1 <danielsousagit@gmail.com>
2021-11-10 13:02:51 -0300

    H2 database, test profile, JPA

--- src/main/java/com/testeweb/course/CourseApplication.java
+++ src/main/java/com/testeweb/course/CourseApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-import com.testeweb.course.entities.User;
+
 
 @SpringBootApplication
 public class CourseApplication {
