7fc22e85f0ec8d76b9b7d1737c03d77549252bd9
Danielscript1 <danielsousagit@gmail.com>
2021-11-15 16:23:58 -0300

    User insert

--- src/main/java/com/testeweb/course/resources/UserResource.java
+++ src/main/java/com/testeweb/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.testeweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.testeweb.course.entities.User;
 import com.testeweb.course.services.UserService;
@@ -29,5 +33,13 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable long id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	//salvando o user
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){ //anotação que avisar que voce vai receber uma Json
+		User usuario = service.insert(obj);
+		//forma correta de inserir um obj no banco de dados
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(usuario.getId()).toUri();
+		return ResponseEntity.created(uri).body(usuario);
 	}
 }
